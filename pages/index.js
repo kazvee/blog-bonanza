3cc7d91276998724ff59035c064d0d6214eed0eb
kazvee <kazveedev@gmail.com>
2024-04-17 09:47:49 -0600

    feat: add login link to home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Blog Bonanza! 😃</h1>
+      <div>
+        <Link href='/api/auth/login'>Login</Link>
+      </div>
     </div>
   );
 }
