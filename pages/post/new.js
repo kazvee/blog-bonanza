f86c84d4526c9110efb6ff6ebcb25fe2fa661e5d
kazvee <kazveedev@gmail.com>
2024-04-18 16:14:35 -0600

    feat: add getServerSideProps to post creation page

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,16 @@
-export default function NewPost() {
+export default function NewPost(props) {
+  console.log('New Post Props:', props);
   return (
     <div>
       <h1>This is the New Post page 📝</h1>
     </div>
   );
 }
+
+export const getServerSideProps = () => {
+  return {
+    props: {
+      test: 'Testing 123! 🧪',
+    },
+  };
+};
