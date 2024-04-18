4bed62effc648d2b43b48ddb8ae3a970a9949714
kazvee <kazveedev@gmail.com>
2024-04-18 16:29:23 -0600

    feat: add auth protection to route for token top-up page

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,3 +1,5 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
 export default function Topup() {
   return (
     <div>
@@ -5,3 +7,9 @@ export default function Topup() {
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
