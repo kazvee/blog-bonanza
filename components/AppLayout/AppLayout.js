fcd10133a1e705441d96971e93a9887f5388687f
kazvee <kazveedev@gmail.com>
2024-04-19 16:45:13 -0600

    feat: add sidebar & styling

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,17 @@
 export const AppLayout = ({ children }) => {
   return (
-    <div>
-      This is the app layout! 🙂
+    <div className='grid grid-cols-[300px_1fr] h-screen max-h-screen'>
+      <div className='flex flex-col text-white overflow-hidden'>
+        <div className='bg-neutral-800'>
+          <div>Logo</div>
+          <div>Button</div>
+          <div>Tokens</div>
+        </div>
+        <div className='flex-1 overflow-auto bg-gradient-to-b from-neutral-800 to-rose-800'>
+          List of Posts
+        </div>
+        <div className='bg-rose-800'>User Details | Logout Button</div>
+      </div>
       <div>{children}</div>
     </div>
   );
