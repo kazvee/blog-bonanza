e22d88c6d7cb62712da1a58ef37a811cdb555a75
kazvee <kazveedev@gmail.com>
2024-04-19 17:47:47 -0600

    feat: update sidebar to display user information

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,9 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const AppLayout = ({ children }) => {
+  const { user } = useUser();
   return (
     <div className='grid grid-cols-[300px_1fr] h-screen max-h-screen'>
       <div className='flex flex-col text-white overflow-hidden'>
@@ -10,7 +15,25 @@ export const AppLayout = ({ children }) => {
         <div className='flex-1 overflow-auto bg-gradient-to-b from-neutral-800 to-rose-800'>
           List of Posts
         </div>
-        <div className='bg-rose-800'>User Details | Logout Button</div>
+        <div className='bg-rose-800'>
+          {' '}
+          {!!user ? (
+            <>
+              <div>
+                <Image
+                  src={user.picture}
+                  alt={user.name}
+                  width={50}
+                  height={50}
+                />
+                {user.email}
+              </div>
+              <Link href='/api/auth/logout'>Logout</Link>
+            </>
+          ) : (
+            <Link href='/api/auth/login'>Login</Link>
+          )}
+        </div>
       </div>
       <div>{children}</div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,8 @@
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Image from 'next/image';
-import Link from 'next/link';
-
 export default function Home() {
-  const { user } = useUser();
-  console.log('USER ➡️', user);
   return (
     <div>
       <h1>Welcome to Blog Bonanza! 😃</h1>
-      <div>
-        {!!user ? (
-          <>
-            <div>
-              <Image
-                src={user.picture}
-                alt={user.name}
-                width={50}
-                height={50}
-              />
-              {user.email}
-            </div>
-            <Link href='/api/auth/logout'>Logout</Link>
-          </>
-        ) : (
-          <Link href='/api/auth/login'>Login</Link>
-        )}
-      </div>
+      <div></div>
     </div>
   );
 }
