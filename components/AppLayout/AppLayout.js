fc82254703b2b77e646ef454583187797240af7f
kazvee <kazveedev@gmail.com>
2024-04-19 19:08:13 -0600

    feat: add styling to sidebar user info section

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -15,20 +15,25 @@ export const AppLayout = ({ children }) => {
         <div className='flex-1 overflow-auto bg-gradient-to-b from-neutral-800 to-rose-800'>
           List of Posts
         </div>
-        <div className='bg-rose-800'>
+        <div className='bg-rose-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2'>
           {' '}
           {!!user ? (
             <>
-              <div>
+              <div className='min-w-[50px]'>
                 <Image
                   src={user.picture}
                   alt={user.name}
                   width={50}
                   height={50}
+                  className='rounded-full'
                 />
-                {user.email}
               </div>
-              <Link href='/api/auth/logout'>Logout</Link>
+              <div className='flex-1'>
+                <div className='font-bold'>{user.email}</div>
+                <Link className='text-sm' href='/api/auth/logout'>
+                  Logout
+                </Link>
+              </div>
             </>
           ) : (
             <Link href='/api/auth/login'>Login</Link>
